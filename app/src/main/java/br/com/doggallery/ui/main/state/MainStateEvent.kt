8e640d1644a9b78d4260ebbcfcdeb9886e69dd9e
Francinildo Alves <franalves1923@gmail.com>
2020-04-03 00:26:34 -0300

    Changed strings language to english instead portuguese and added clear filter button label and its description

--- app/src/main/java/br/com/doggallery/ui/main/state/MainStateEvent.kt
+++ app/src/main/java/br/com/doggallery/ui/main/state/MainStateEvent.kt
@@ -2,7 +2,7 @@ package br.com.doggallery.ui.main.state
 
 sealed class MainStateEvent {
     class GetDogsRandomly: MainStateEvent()
-    class SetDogBreed(val selectedBreed: String): MainStateEvent()
+    class SetDogBreed(val selectedBreed: String?): MainStateEvent()
     class GetDogsByBreed(val breed: String): MainStateEvent()
     class GetAllBreeds: MainStateEvent()
     class None: MainStateEvent()
